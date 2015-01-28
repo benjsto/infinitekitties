ae0964577724dac249c8c7d4718e07f94184acd0
Stockwell, Benjamin <Ben.Stockwell@bestbuy.com>
2015-01-28 17:56:41 -0600

    Tweak project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject infinitekitties "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Infinite Kitties"
+  :url "http://infinitekitties.com"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [lib-noir "0.9.5"]
                  [ring-server "0.3.1"]
@@ -8,8 +8,7 @@
                  [selmer "0.7.7"]
                  [com.taoensso/timbre "3.3.1"]
                  [com.taoensso/tower "3.0.2"]
-                 [markdown-clj "0.9.58"
-                  :exclusions [com.keminglabs/cljx]]
+                 [markdown-clj "0.9.58" :exclusions [com.keminglabs/cljx]]
                  [environ "1.0.0"]
                  [im.chit/cronj "1.4.3"]
                  [noir-exception "0.2.3"]
